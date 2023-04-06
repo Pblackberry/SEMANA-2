6e01dc8e73a85ec6de7d22fa0e8ada4ae7f6282c
tomas.lopez <tomaslopezandrade2004@gmail.com>
2023-04-06 15:33:00 -0500

    inicio

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    char respuesta;
+    printf("hola beibigirl, como andas\n");
+    scanf("%c", &respuesta);
+    
+    
+
+    return 0;
+}
